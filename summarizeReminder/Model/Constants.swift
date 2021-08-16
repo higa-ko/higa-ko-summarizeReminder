ee6d6a1fafa72c1d09c839191b2c6effca210c4d
葡萄酒 <>
2021-08-16 18:00:00 +0900

    インポートビューのセルを配置

--- summarizeReminder/Model/Constants.swift
+++ summarizeReminder/Model/Constants.swift
@@ -7,10 +7,16 @@
 
 struct K {
 
-
     struct CellIdentifier {
         static let CategoryCell = "CategoryCell"
         static let TaskyCell = "TaskCell"
+        static let NewCategoryCheckCell = "NewCategoryCheckCell"
+        static let CategoryInputCell = "CategoryInputCell"
+        static let CategorySelectCell = "CategorySelectCell"
+        static let NoticeCheckCell = "NoticeCheckCell"
+        static let TimeSelectCell = "TimeSelectCell"
+        static let RepeatSelectCell = "RepeatSelectCell"
+        static let TaskAddCell = "TaskAddCell"
     }
     
     struct SegueIdentifier {
